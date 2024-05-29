772d8af578e0ee8a8c94efa1380edb2b98da9a06
Md Towhidul Alam <158472221+mdtowhid98@users.noreply.github.com>
2024-05-29 09:53:23 +0600

    29.05.2024

---
+++ TowhidStore/src/util/DbUtil.java
@@ -0,0 +1,28 @@
+
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class DbUtil {
+    private Connection connection=null;
+    private String url="jdbc:mysql://localhost:3306/towhidstore";
+    private String user="root";
+    private String password="1234";
+    private String driver="com.mysql.cj.jdbc.Driver";
+    
+    public Connection getCon() throws ClassNotFoundException{
+    Class.forName(driver);
+    
+        try {
+            connection=DriverManager.getConnection(url, user, password);
+        } catch (SQLException ex) {
+            Logger.getLogger(DbUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return connection;
+    }
+}
